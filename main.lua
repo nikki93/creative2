c184bf603b82f90458656633c946565078d7638c
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2019-06-26 22:28:52 -0700

    coding!

--- main.lua
+++ main.lua
@@ -4,67 +4,103 @@ local simulsim = require 'https://raw.githubusercontent.com/nikki93/simulsim/2f7
 
 local game = simulsim.defineGame()
 
+local compileRule, callRule
+do
+    local compileCache = {}
+
+    function compileRule(rule)
+        local cached = compileCache[rule.code]
+        if cached then
+            return cached
+        end
+
+        local compiled, err = load('local self = ...\n' .. rule.code, rule.id, 't', _G)
+        compileCache[rule.code] = compiled or err
+        if err then
+            print(err)
+        end
+        return compiled
+    end
+
+    function callRule(rule, ...)
+        local compiled = compileRule(rule)
+        if type(compiled) == 'function' then
+            local succeeded, err = pcall(compiled, ...)
+            if not succeeded then
+                print(err)
+            end
+        end
+    end
+end
+
+do
+    local function compareRules(rule1, rule2)
+        return (rule1.priority or 0) < (rule2.priority or 0)
+    end
+
+    function sortRules(rules)
+        local sorted = {}
+        for id, rule in pairs(rules) do
+            table.insert(sorted, rule)
+        end
+        table.sort(sorted, compareRules)
+        return sorted
+    end
+end
+
 function game.load(self)
-    self.data.backgroundColor = { 0.1, 0.1, 0.1 }
+    self.data.rules = {}
 end
 
 function game.update(self, dt)
-    for _, entity in ipairs(self.entities) do
-        local inputs = self:getInputsForClient(entity.clientId) or {}
-        local moveX = (inputs.right and 1 or 0) - (inputs.left and 1 or 0)
-        local moveY = (inputs.down and 1 or 0) - (inputs.up and 1 or 0)
-        entity.x = math.min(math.max(0, entity.x + 200 * moveX * dt), 380)
-        entity.y = math.min(math.max(0, entity.y + 200 * moveY * dt), 380)
+    local sortedRules = sortRules(self.data.rules)
+    for _, rule in ipairs(sortedRules) do
+        if rule.type == 'update' then
+            callRule(rule, self)
+        end
     end
 end
 
 function game.handleEvent(self, eventType, eventData)
-    if eventType == 'spawn-player' then
-        self:spawnEntity({
-            clientId = eventData.clientId,
-            x = eventData.x,
-            y = eventData.y,
-            width = 20,
-            height = 20,
-            color = eventData.color
-        })
-    elseif eventType == 'despawn-player' then
-        self:despawnEntity(self:getEntityWhere({ clientId = eventData.clientId }))
+    if eventType == 'set-rule' then
+        self.data.rules[eventData.rule.id] = eventData.rule
     end
 end
 
 local network, server, client = simulsim.createGameNetwork(game, { mode = 'localhost' })
 
-function server.clientconnected(self, client)
-    self:fireEvent('spawn-player', {
-        clientId = client.clientId,
-        x = 100 + 200 * math.random(),
-        y = 100 + 200 * math.random(),
-        color = { math.random(), 1, math.random() }
+function server.load(self)
+    self:fireEvent('set-rule', {
+        rule = {
+            id = math.random(2000),
+            priority = 0,
+            type = 'draw',
+            description = 'Draw a circle!',
+            code = [[
+L.circle('fill', 200, 200, 40)
+]],
+        },
     })
+end
+
+function server.clientconnected(self, client)
 end
 
 function server.clientdisconnected(self, client)
-    self:fireEvent('despawn-player', { clientId = client.clientId })
 end
 
+local clientSelf
 function client.update(self, dt)
-    self:setInputs({
-        up = L.keyboard.isDown('w') or L.keyboard.isDown('up'),
-        left = L.keyboard.isDown('a') or L.keyboard.isDown('left'),
-        down = L.keyboard.isDown('s') or L.keyboard.isDown('down'),
-        right = L.keyboard.isDown('d') or L.keyboard.isDown('right')
-    })
+    clientSelf = self
 end
 
 function client.draw(self)
-    L.setColor(self.game.data.backgroundColor)
-    L.rectangle('fill', 0, 0, 400, 400)
-    for _, entity in ipairs(self.game.entities) do
-        L.setColor(entity.color)
-        L.rectangle('fill', entity.x, entity.y, entity.width, entity.height)
+    local sortedRules = sortRules(self.game.data.rules)
+    for _, rule in ipairs(sortedRules) do
+        if rule.type == 'draw' then
+            callRule(rule, self.game)
+        end
     end
-    L.setColor(1, 1, 1)
     if self:isConnecting() then
         L.print('Connecting...', 3, 3)
     elseif not self:isConnected() then
@@ -76,10 +112,90 @@ function client.draw(self)
     end
 end
 
-function castle.uiupdate()
-    L.ui.markdown([[
-## Hello, world!
+do
+    local selectedRuleId
 
-Hey there! :)
-    ]])
+    function castle.uiupdate()
+        local self = clientSelf
+        if not (self and self.game) then
+            return
+        end
+
+        -- Button to add new rule
+        if L.ui.button('new rule') then
+            self:fireEvent('set-rule', {
+                rule = {
+                    id = math.random(2000),
+                    priority = 0,
+                    type = 'draw',
+                    description = 'draw a circle!',
+                    code = [[
+        L.circle('fill', ]] .. math.random(0, L.getWidth()) .. [[, ]] .. math.random(0, L.getHeight()) .. [[, 40)
+        ]],
+                },
+            })
+        end
+
+        -- Dropdown to select rule
+        local selectedRule
+        do
+            local selectedRuleLine
+            local ruleLines = {}
+            local ruleLineToRuleId = {}
+            for id, rule in pairs(self.game.data.rules) do
+                local ruleLine = rule.description .. ' (' .. rule.id .. ')'
+                table.insert(ruleLines, ruleLine)
+                ruleLineToRuleId[ruleLine] = id
+                if id == selectedRuleId then
+                    selectedRuleLine = ruleLine
+                end
+            end
+            local selectedRuleLine = L.ui.dropdown('rule', selectedRuleLine, ruleLines, {
+                placeholder = 'select a rule...'
+            })
+            selectedRuleId = ruleLineToRuleId[selectedRuleLine]
+
+            selectedRule = self.game.data.rules[selectedRuleId]
+            if not selectedRule then
+                selectedRuleId = nil
+            end
+        end
+
+        -- Editor for selected rule
+        if selectedRule then
+            local newRule = {}
+            for k, v in pairs(selectedRule) do
+                newRule[k] = v
+            end
+
+            newRule.priority = L.ui.numberInput('priority', selectedRule.priority)
+
+            newRule.type = L.ui.dropdown('type', selectedRule.type, {
+                'draw', 'update', 'ui',
+            })
+
+            newRule.description = L.ui.textInput('description', selectedRule.description, {
+                maxLength = 80,
+            })
+
+            newRule.code = L.ui.codeEditor('code', selectedRule.code)
+
+            local changed = false
+            for k, v in pairs(newRule) do
+                if selectedRule[k] ~= newRule[k] then
+                    changed = true
+                    break
+                end
+            end
+            for k, v in pairs(selectedRule) do
+                if selectedRule[k] ~= newRule[k] then
+                    changed = true
+                    break
+                end
+            end
+            if changed then
+                self:fireEvent('set-rule', { rule = newRule })
+            end
+        end
+    end
 end
